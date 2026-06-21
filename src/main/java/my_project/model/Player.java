c5ab093be9ee0cd3ccc46d060ebcd8c6bb92cc22
Elias <eliasqaddouri9@gmail.com>
2026-06-21 16:26:24 +0200

    TrapSelector

---
+++ src/main/java/my_project/model/Player.java
@@ -0,0 +1,18 @@
+package my_project.model;
+
+public class Player {
+
+    private int hp;
+
+    public Player() {
+        hp = 150;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int pHp) {
+        hp = pHp;
+    }
+}
